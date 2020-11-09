fb627a695188b7cafad04745f9634ede3d69623f
John McKie <john.mckie@remarkable.net>
2020-11-09 19:29:14 +0000

    start of the layout

---
+++ script.js
@@ -0,0 +1,6 @@
+const main = document.querySelector('#main');
+const addUser = document.querySelector('#add-user');
+const double = document.querySelector('#double');
+const showMillionaires = document.querySelector('#show-millionaires');
+const sort = document.querySelector('#sort');
+const calculateWealth = document.querySelector('#calculate-wealth');
